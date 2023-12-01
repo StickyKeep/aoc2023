80b47a5d17ada84f3628f04e55e0bb2e82dc34a4
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-01 18:30:03 +0100

    Merge test

--- day1.py
+++ day1.py
@@ -1,38 +1,12 @@
-<<<<<<< HEAD
-=======
 # Rewritten to only find first and last number
 # Still very spaghetti :) 
 
->>>>>>> test
 with open('input.in') as f:
     content = [x.strip() for x in f.readlines()]
 
 nums = {"one": 1, "two": 2, "three": 3, "four": 4,
         "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
-<<<<<<< HEAD
-sum = 0
-
-for line in content:
-    string = ""
-    i = 0
-    while i < len(line):
-        if line[i].isdigit():
-            string += str(line[i])
-            i += 1
-        else:
-            for number, value in nums.items():
-                if line[i:].startswith(number):
-                    string += str(value)
-                    i += 1
-
-            else:
-                i += 1
-
-    sum += int(string[0] + string[-1])
-
-print(sum)
-=======
 def find_digits(s, number_dict, reverse=False):
     if reverse == False:
         for i in range(len(s)):
@@ -55,14 +29,11 @@ total_sum = 0
 
 for line in content:
     n1 = find_digits(line, nums)
-    print(n1)
     n2 = find_digits(line, nums, reverse=True)
-    print(n2)
     total_sum += int(n1 + n2)
 
 print(total_sum)
 
 # Ans: 54676
 
->>>>>>> test
 

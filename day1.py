819e047f896651a23dfffc543bac220b0861c735
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-01 18:28:18 +0100

    Rewrite to only find first and last num

--- day1.py
+++ day1.py
@@ -1,27 +1,41 @@
-with open('day1_input.txt') as f:
+# Rewritten to only find first and last number
+# Still very spaghetti :) 
+
+with open('input.in') as f:
     content = [x.strip() for x in f.readlines()]
 
 nums = {"one": 1, "two": 2, "three": 3, "four": 4,
         "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
-sum = 0
+def find_digits(s, number_dict, reverse=False):
+    if reverse == False:
+        for i in range(len(s)):
+            if s[i].isdigit():
+                return str(s[i])
+            else:
+                for number, value in number_dict.items():
+                    if s[i:].startswith(number):
+                        return str(value)
+    else:
+        for i in range(len(s))[::-1]:
+            if s[i].isdigit():
+                return str(s[i])
+            else:
+                for number, value in number_dict.items():
+                    if s[i:].startswith(number):
+                        return str(value)
+                    
+total_sum = 0
 
 for line in content:
-    string = ""
-    i = 0
-    while i < len(line):
-        if line[i].isdigit():
-            string += str(line[i])
-            i += 1
-        else:
-            for number, value in nums.items():
-                if line[i:].startswith(number):
-                    string += str(value)
-                    i += 1
+    n1 = find_digits(line, nums)
+    print(n1)
+    n2 = find_digits(line, nums, reverse=True)
+    print(n2)
+    total_sum += int(n1 + n2)
 
-            else:
-                i += 1
+print(total_sum)
 
-    sum += int(string[0] + string[-1])
+# Ans: 54676
 
-print(sum)
+

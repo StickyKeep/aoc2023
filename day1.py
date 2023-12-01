84f3a94090d40e4250e4e31d68ec7d7ebcfc2282
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-01 14:11:03 +0100

    2 star

--- day1.py
+++ day1.py
@@ -1,26 +1,27 @@
-# gogo
-
 with open('day1_input.txt') as f:
     content = [x.strip() for x in f.readlines()]
 
+nums = {"one": 1, "two": 2, "three": 3, "four": 4,
+        "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
 sum = 0
-string = ""
-
-content2 = []
-
 
 for line in content:
-    ints = []
-    for i in line: 
-        try:
-            int(i)
-            ints.append(i)
-        except ValueError:
-            pass
-    content2.append(ints)
+    string = ""
+    i = 0
+    while i < len(line):
+        if line[i].isdigit():
+            string += str(line[i])
+            i += 1
+        else:
+            for number, value in nums.items():
+                if line[i:].startswith(number):
+                    string += str(value)
+                    i += 1
 
-for numbs in content2:
-    sum += int(numbs[0] + numbs[-1])
+            else:
+                i += 1
+
+    sum += int(string[0] + string[-1])
 
 print(sum)
-

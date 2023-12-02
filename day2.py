17870d728eab81e161e293ce18e2cd2e6976a363
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-02 14:12:01 +0100

    2 star

--- day2.py
+++ day2.py
@@ -20,8 +20,27 @@ for game, groups in enumerate(data, 1):
     else:
         tsum += game
 
-print(tsum)
+
+# Quick copy paste and edit for part 2,
+# to find the minimum numbers of each color needed for each game
+
+power_sum = 0 
+
+for game, groups in enumerate(data, 1):
+    color_map = {"red": 0, "green": 0, "blue": 0}
+    for group in groups:
+        
+        for pair in group:
+            num, color = pair.split()
+            if color_map[color] < int(num):
+                color_map[color] = int(num)
+    r, g, b = color_map.values()
+    power_sum += r * g * b
+
+
+print(f"Sum of indexes of possible games: {tsum}")
+print(f"Power sum of minimum number of colors needed: {power_sum}")
 
 # 2169
-
+# 60948
 

fce9a9141560dd89aee3ef63c6b352f687de156b
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-06 19:03:26 +0100

    Add input parsing

--- day5.py
+++ day5.py
@@ -1,4 +1,18 @@
 with open("input.in", "r") as f:
+    maps = []
     seeds = [int(x) for x in f.readline().split(": ")[1].split()]
+    
+    for line in f:
+        line = line.strip("")
+        if line == "\n":
+            continue
+        if line.endswith("map:\n"):
+            continue
+        else:
+            maps.append([int(x) for x in line.split()])
+
 
 print(seeds)
+print(maps)
+
+# \n

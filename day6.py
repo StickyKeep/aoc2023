640986925802d9b0a5dc9b81d305a934fac599f0
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-06 06:56:48 +0100

    2 star

--- day6.py
+++ day6.py
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2023/day/6
+
 with open("input.in", "r") as f:
     times = [int(x) for x in f.readline().split(": ")[1].split()]
     distance = [int(x) for x in f.readline().split(": ")[1].split()]
@@ -16,4 +18,19 @@ for i, time in enumerate(times):
 
 print(number_of_wins)
             
+# Copy paste and reuse for part 2: 
+# "There's really only one race - ignore the spaces between the numbers on each line."
 
+time = int(''.join(map(str, times)))
+distance = int(''.join(map(str, distance)))
+
+wins2 = 0
+for seconds_pressed in range(1, time):
+    distance2 = (time - seconds_pressed) * seconds_pressed
+    if distance2 > distance:
+        wins2 += 1
+
+print(wins2)    
+    
+# 138915
+# 27340847

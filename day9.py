c3c4d3448f7c83541ed6c20a60007e67c99d32e4
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-09 19:37:12 +0100

    1 star

--- day9.py
+++ day9.py
@@ -1,6 +1,17 @@
 with open("input.in") as f:
     nums = [list(map(int,line.strip().split())) for line in f]
 
-for numbers in nums:
-    for n in numbers:
-        print(n)
+
+
+def find_differences(nums_list):
+    if all(number == 0 for number in nums_list):
+        return 0
+    differences = [y- x for x, y in zip(nums_list, nums_list[1:])]
+    difference = find_differences(differences)
+    return nums_list[-1] + difference
+
+sum = 0
+for going_out_of_variable_names in nums:
+    sum += find_differences(going_out_of_variable_names)
+
+print(sum)

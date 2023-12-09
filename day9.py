fb4cc85334e400ec9e4cac16ef192218d371e4d1
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-12-09 19:42:10 +0100

    Formatting

--- day9.py
+++ day9.py
@@ -1,17 +1,19 @@
 with open("input.in") as f:
-    nums = [list(map(int,line.strip().split())) for line in f]
+    nums = [list(map(int, line.strip().split())) for line in f]
 
 
 def find_differences(nums_list):
     if all(number == 0 for number in nums_list):
         return 0
-    differences = [y- x for x, y in zip(nums_list, nums_list[1:])]
+    differences = [y - x for x, y in zip(nums_list, nums_list[1:])]
     difference = find_differences(differences)
     return nums_list[-1] + difference
 
 sum = 0
-for going_out_of_variable_names in nums:
-    sum += find_differences(going_out_of_variable_names)
+
+# Iterate over input numbers:
+for running_out_of_variable_names in nums:
+    sum += find_differences(running_out_of_variable_names)
 
 print(f"Part 1 sum: {sum}")
 
@@ -25,9 +27,9 @@ def find_differences_pt2(nums_list):
     difference = find_differences_pt2(differences)
     return nums_list[0] - difference
 
-
 sum = 0
-for going_out_of_variable_names in nums:
-    sum += find_differences_pt2(going_out_of_variable_names)
+
+for running_out_of_variable_names in nums:
+    sum += find_differences_pt2(running_out_of_variable_names)
 
 print(f"Part 2 sum: {sum}")
